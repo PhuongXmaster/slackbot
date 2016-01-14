dce6a860ddfa2b708eab2d4ad1b978f5d158da27
Ngoc Nguyen <nthngoc277@gmail.com>
2016-01-14 20:21:52 +0700

    Customize slack message

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,3 +1,11 @@
 class LanguageBot < SlackRubyBot::Bot
-  # leave it empty for now
-end
+  command 'hallo' do |client, data, match|
+    client.say(text: "hello <#{data.user}>!", channel: data.channel)
+  end
+
+  command 'hello' do |client, data, match|
+     I18n.locale = :vi
+     text = I18n.t "hello", user: data.user
+     client.say(text: text, channel: data.channel)
+   end
+end 
